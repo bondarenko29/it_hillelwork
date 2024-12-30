c51db7c2ef6165e2a4752261b16891f0778fa364
IrynaBond <fm.ub15b.bondarenko@gmail.com>
2024-12-30 11:51:13 +0200

    log function

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+
 function showMessage() {
     alert('Hello');
 }
 function showConsole() {
     console.log('Hi');
 }
+function addSum() {
+    let a = 5;
+    let b = 7;
+    let sum = a + b;
+    console.log(`Сума ${a} + ${b} = `, sum);
+}
+addSum();
